4ee7d713268b10e51a69b8fcf0c87f6c22658bce
Nuno <nuno.castro@sequoia.pt>
2026-05-14 12:05:30 +0100

    Historico Cartão removed

--- src/main/java/pt/sequoia/standByTool/repositories/HistoricoCartaoRepository.java
+++
@@ -1,7 +0,0 @@
-package pt.sequoia.standByTool.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pt.sequoia.standByTool.models.HistoricoCartao;
-
-public interface HistoricoCartaoRepository extends JpaRepository<HistoricoCartao, Long> {
-}
