b9e714b8dd669a3908d5d813c69e8023ece7d5ca
Nuniko04 <nuno.castro@sequoiaconsulting.pt>
2026-05-25 14:57:55 +0100

    Delete src/main/java/pt/sequoia/standByTool/repositories/CartaoRepository.java

--- src/main/java/pt/sequoia/standByTool/repositories/CartaoRepository.java
+++
@@ -1,7 +0,0 @@
-package pt.sequoia.standByTool.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pt.sequoia.standByTool.models.Cartao;
-
-public interface CartaoRepository extends JpaRepository<Cartao, Long> {
-}
