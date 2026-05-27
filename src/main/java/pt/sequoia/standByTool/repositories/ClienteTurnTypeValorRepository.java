947f8b4ceb3e9c63bd5b2c4be4dc0481984944f3
Nuno <nuno.castro@sequoia.pt>
2026-05-27 10:46:35 +0100

    Changed errors in FinanceService

--- src/main/java/pt/sequoia/standByTool/repositories/ClienteTurnTypeValorRepository.java
+++ src/main/java/pt/sequoia/standByTool/repositories/ClienteTurnTypeValorRepository.java
@@ -5,10 +5,11 @@ import pt.sequoia.standByTool.models.ClienteTurnTypeValor;
 import pt.sequoia.standByTool.models.ServicoCliente;
 import pt.sequoia.standByTool.models.TurnType;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ClienteTurnTypeValorRepository extends JpaRepository<ClienteTurnTypeValor, UUID> {
 
     // A query mágica que o Spring cria automaticamente para encontrar o preço exato!
-    ClienteTurnTypeValor findByClienteAndTurnType(ServicoCliente cliente, TurnType turnType);
+    Optional<ClienteTurnTypeValor> findByClienteAndTurnType(ServicoCliente cliente, TurnType turnType);
 }
